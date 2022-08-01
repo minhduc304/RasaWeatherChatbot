1de1d8411911ab04d732a9e59f7a58aff72cabce
minhduc <mihducv42@gmail.com>
2022-08-01 17:12:15 +0700

    Deleted unnecessary HTML elements and added internal for weather data.

--- actions/actions.py
+++ actions/actions.py
@@ -10,8 +10,8 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import json
 import actions.modules.WeatherSiteCrawler.sitecrawler as sc
-
 
 class ActionGetWeather(Action):
 
@@ -21,13 +21,26 @@ class ActionGetWeather(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
+        path_to_file = 'actions/modules/WeatherSiteCrawler/weather_info.json'
+
+        file_ = open(path_to_file, 'w+')
         
         for blob in tracker.latest_message['entities']:
             if blob['entity'] == 'city':
-                city_name = blob['value']
-                city_id = sc.choose_city(city_name, sc.city_dict)
-                dispatcher.utter_message(sc.produce_output(sc.return_weather(city_id)))
+                city_name = blob['value']   
+                if city_name in file_:
+                    dispatcher.utter_message(dictionary[city_name])
+                else:
+                    city_id = sc.choose_city(city_name, sc.city_dict)
+                    output_ = sc.produce_output(sc.return_weather(city_id))
+                    temp_dict = {}
+                    temp_dict[city_name] = output_
+                    json.dump(temp_dict, file_, indent=4,  separators=(',',': '))
+                    
+
+                    dispatcher.utter_message(output_)
+                    file_.close()
+            
             else:
                 dispatcher.utter_message("Sorry. I don't recognize that city. Can you try again? ")
                 
